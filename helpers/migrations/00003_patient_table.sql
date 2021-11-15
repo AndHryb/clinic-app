bd9c2bc0e7c4bda123990ac4d9f3ada4a7c89f2f
andrei.hrybouski <andryigr@mail.ru>
2021-11-16 01:30:59 +0300

    fix sql table naming

--- helpers/migrations/00003_patient_table.sql
+++ helpers/migrations/00003_patient_table.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS patients (
   name varchar(255) NOT NULL,
   gender varchar(255) NOT NULL,
   birthday date NOT NULL,
-  userId varchar(255) NOT NULL,
+  user_id varchar(255) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (userId) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
   ON UPDATE CASCADE
   ON DELETE CASCADE
 );
